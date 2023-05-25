89f24c495fd7d36f38d0d249b0f260456acfcf2f
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-05-25 18:40:07 -0300

    atualizando requisição http e melhorando tipagem das classes

--- fortune_cookie/lib/components/cookie_advice.dart
+++ fortune_cookie/lib/components/cookie_advice.dart
@@ -10,12 +10,11 @@ class CookieAdvice extends StatefulWidget {
 }
 
 class _CookieState extends State<CookieAdvice> {
-
-  Future<List> catchAdvice() async {
+  Future<Map<String, dynamic>> catchAdvice() async {
     var url = Uri.parse('https://api.adviceslip.com/advice');
     var response = await http.get(url);
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
       throw Exception('Error obtain data from the server');
@@ -24,26 +23,29 @@ class _CookieState extends State<CookieAdvice> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List>(
-      future: catchAdvice(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(child: Text('Deu erro carai'));
-        } 
-
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index){
-              return Text(snapshot.data![index]['advice']); 
-            },
+    return FutureBuilder<Map<String, dynamic>>(
+        future: catchAdvice(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Deu erro mano'));
+          } else if (snapshot.hasData) {
+            return GestureDetector(
+              onTap: () {
+                
+              },
+              child: Container(
+                height: 200,
+                color: Colors.red,
+                child: Image.asset(
+                  'assets/images/fortune_cookie.png',
+                  fit: BoxFit.cover,
+                ),
+              ));
+          }
+          return Center(
+            child: CircularProgressIndicator(),
           );
-        }
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-    );
+        });
   }
 }
 
